40c4b6d6fd93b0ec99beb31f85edee4b7d275fd3
FINISinstrument <FINISinstrument@gmail.com>
2019-03-29 17:29:56 -0600

    IMU.cs
    class for IMU data

---
+++ finisGUITest2/IMU.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Allows access to data types within the VectorNav .NET Library.
+using VectorNav.Sensor;
+using VectorNav.Math;
+using VectorNav.Protocol.Uart;
+
+namespace FinisGUI
+{
+    class IMU
+    {
+    }
+}
