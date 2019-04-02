fedb38cacede78525c2912f1a2a810558a74733e
FINISinstrument <FINISinstrument@gmail.com>
2019-04-02 11:07:31 -0600

    Update IMU.cs
    Added Constructor, and function for receiveing time info from IMU(in progress)

--- finisGUITest2/IMU.cs
+++ finisGUITest2/IMU.cs
@@ -11,17 +11,38 @@ namespace FinisGUI
 {
     public class IMU
     {
+
+        VnSensor vs;
+        string sensorPort;
+        uint sensorBaudrate;
+
+        public IMU(string sensorPort, uint sensorBaudrate)
+        {
+            vs = new VnSensor();
+            this.sensorPort = sensorPort;
+            this.sensorBaudrate = sensorBaudrate;
+        }
+        
+
+        
         //const string sensorPort = "COM12";
         //const UInt32 sensorBaudrate = 115200;
-        public bool ConnectSensor(string sensorPort, UInt32 sensorBaudrate = 115200)
+        public bool ConnectSensor()
         {
-            var vs = new VnSensor();
+            //var vs = new VnSensor();
             vs.Connect(sensorPort, sensorBaudrate);
             if (vs.IsConnected)
             {
                 return true;
             }
             return false;
+        }
+
+        public bool GetTime()
+        {
+            //vs.ReadGpsSolutionLla;
+
+            return true;
         }
         
 
